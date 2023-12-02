6f5f79967c145e8ab0e25e82ed31ff0244f58f51
alan-palacios <apyolt@gmail.com>
2023-12-01 21:24:50 -0600

    add songs

--- keyboards/equals48/keymaps/alply/keymap.c
+++ keyboards/equals48/keymaps/alply/keymap.c
@@ -19,18 +19,22 @@ enum custom_keycodes {
     COMMIT,
     ADD,
     CLEAR_EEPROM,
+    RICK,
+    RUDOLPH
 };
+float the_song[][2] = SONG(THE_SONG);
+float rudolph_song[][2] = SONG(RUDOLPH_MELODY);
 
-float enter_sound[][2] = {
+float git_add_sound[][2] = {
     {NOTE_G5, 4}, // G5 note, 1/4th duration
 };
+
+float git_commit_sound[][2] = {
+    {NOTE_C5, 4}, // G5 note, 1/4th duration
+};
+
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     switch (keycode) {
-      case KC_ENTER:
-          if (record->event.pressed) {
-            PLAY_SONG(enter_sound);
-          }
-          break;
       case HEART:
           if (record->event.pressed) {
               SEND_STRING("<3 ");
@@ -39,16 +43,28 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
       case ADD:
           if (record->event.pressed) {
               SEND_STRING("git add .");
+              PLAY_SONG(git_add_sound);
           }
           break;
       case COMMIT:
           if (record->event.pressed) {
               SEND_STRING("git commit -m \"");
+              PLAY_SONG(git_commit_sound);
           }
           break;
       case CLEAR_EEPROM:
           if (record->event.pressed) {
               eeconfig_init();
+          }
+          break;
+      case RICK:
+          if (record->event.pressed) {
+              PLAY_SONG(the_song);
+          }
+          break;
+      case RUDOLPH:
+          if (record->event.pressed) {
+              PLAY_SONG(rudolph_song);
           }
           break;
     }
@@ -126,9 +142,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 
   [_SYSTEM] = LAYOUT_ortho_4x12( /* System functions */
-    QK_BOOT,   LGUI(KC_1),   LGUI(KC_2), LGUI(KC_3),		   KC_MPLY, ADD,     COMMIT,  _______, _______, _______, _______, KC_SLEP,
+    QK_BOOT,      LGUI(KC_1),   LGUI(KC_2), LGUI(KC_3),		   KC_MPLY, ADD,     COMMIT,  _______, _______, _______, _______, KC_SLEP,
     CLEAR_EEPROM, LALT(KC_SPC), KC_PSCR,	   LCTL(LSFT(KC_M)), KC_VOLD, KC_VOLU, HEART,   _______, _______, _______, _______, _______,
-    _______, _______,	     _______,	   _______,			     _______, _______, _______, _______, _______, _______, _______, _______,
-    _______, _______,	     _______,    _______, 		     _______, _______, _______, _______, _______, _______, _______, _______
+    RICK,         _______,	     _______,	   _______,			     _______, _______, _______, _______, _______, _______, _______, _______,
+    RUDOLPH,      _______,	     _______,    _______, 		     _______, _______, _______, _______, _______, _______, _______, _______
   )
 };
